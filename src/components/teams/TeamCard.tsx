d23fe7af574d263419bec492773a8fb3290dc7b6
er2klc <info@er2klc.de>
2024-12-30 22:59:10 +0100

    Update TeamCard.tsx
    navigate(`/teams/${team.slug}`); URL FIX

--- src/components/teams/TeamCard.tsx
+++ src/components/teams/TeamCard.tsx
@@ -39,7 +39,7 @@ export const TeamCard = ({
         (e.target as HTMLElement).closest('[role="dialog"]')) {
       return;
     }
-    navigate(`/unity/team/${team.slug}`);
+    navigate(`/teams/${team.slug}`);
   };
 
   const isTeamOwner = user?.id === team.created_by;
