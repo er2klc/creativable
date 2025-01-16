15c3137cfd6fb77dacb4eadea56aca1b829820ed
er2klc <info@er2klc.de>
2025-01-16 15:29:12 +0100

    Update PlatformCard.tsx

--- src/components/elevate/card/PlatformCard.tsx
+++ src/components/elevate/card/PlatformCard.tsx
@@ -27,7 +27,7 @@ export const PlatformCard = ({ platform, onDelete }: PlatformCardProps) => {
         <PlatformCardImage platform={platform} />
         <div className="absolute inset-0 bg-gradient-to-t from-[#222]/95 to-transparent" />
         <div className="absolute bottom-4 left-0 right-0 text-center">
-          <h3 className="text-xl font-orbitron text-white/90">{platform.name}</h3>
+          <h3 className="text-xl text-white/90">{platform.name}</h3>
         </div>
         <PlatformCardActions 
           platformId={platform.id}
