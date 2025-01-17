cfd22efe098b5e1c6a05b35a5bb0cb04e297c5b6
er2klc <info@er2klc.de>
2025-01-17 11:10:05 +0100

    Update SidebarMenuSection.tsx

--- src/components/layout/sidebar/SidebarMenuSection.tsx
+++ src/components/layout/sidebar/SidebarMenuSection.tsx
@@ -46,9 +46,9 @@ export const SidebarMenuSection = ({
             <SidebarMenuItem key={item.title}>
               <SidebarMenuButton asChild>
                 <a href={item.url} className="flex items-center gap-3 relative px-4 py-5 text-gray-300 bg-transparent hover:text-white hover:bg-transparent focus:bg-transparent active:bg-transparent transition-all duration-200 group/item">
-                  <div className="relative pr-4">
+                  <div className="relative">
                     {item.showProgress ? (
-                    <div className="relative flex items-center justify-center w-8 h-8">
+                    <div className="relative pr-4 flex items-center justify-center w-8 h-8">
   <svg 
     className="absolute w-full h-full" viewBox="0 0 36 36">
     <path
