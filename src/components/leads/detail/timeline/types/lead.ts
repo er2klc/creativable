313fa79c63c2ba71282864b77c2ed66893e396d2
er2klc <info@er2klc.de>
2025-02-01 16:25:52 +0100

    Update lead.ts

--- src/components/leads/detail/timeline/types/lead.ts
+++ src/components/leads/detail/timeline/types/lead.ts
@@ -5,7 +5,7 @@ export type LeadWithRelations = Tables<"leads"> & {
   platform: Platform;
   messages: Tables<"messages">[];
   tasks: Tables<"tasks">[];
-  notes: Tables<"notes">[];
+  notes: Note[];
   lead_files: Tables<"lead_files">[];
   parent_id?: string | null;
   level?: number | null;
